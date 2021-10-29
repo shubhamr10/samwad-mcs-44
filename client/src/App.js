93e4a0c0c80be4b66fb7882649ad25cd6b3a0a5d
shubhamr10 <shubhamrauniyar10@gmail.com>
2021-10-29 19:32:39 +0530

    Added Create Profile Component

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import { loadUser } from './actions/auth.action';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -36,6 +37,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             </Switch>
           </section>
         </Router>
--- client/src/actions/profile.action.js
+++ client/src/actions/profile.action.js
@@ -11,13 +11,41 @@ export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
         dispatch({
-            type:GET_PROFILE,
-            payload:res.data
+            type: GET_PROFILE,
+            payload: res.data
         })
     } catch (error) {
         dispatch({
-            type:PROFILE_ERROR,
-            payload:{ msg:error.response.statusText, status: error.response.status }
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        })
+    }
+}
+
+/**
+ * @description: CREATE OR UPDATE THE PROFILE
+ */
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = { headers:{ 'Content-Type': 'application/json' }};
+
+        const res = await axios.post('/api/profile', formData, config);
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+        dispatch(setAlert(edit ? 'Profile updated!':'Profile Created!','success'));
+        if(!edit){
+            history.push('/dashboard');
+        }
+    } catch (error) {
+        const errors = error.response.data.errors;
+        if (errors) {
+            errors.map(err => dispatch(setAlert(err.msg, 'danger')));
+        }
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
         })
     }
 }
