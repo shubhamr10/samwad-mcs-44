69a8f6ff230eb69684b9cde2a28843a98eead9b3
shubhamr10 <shubhamrauniyar10@gmail.com>
2021-11-01 08:20:38 +0530

    Final changes for prod

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require();
 
 // Connect the database
 connectDB();
 
 // INIT middleware for parsing body
-app.use(express.json({ extended :false }));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API RUNNING!!'));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+    // SET STATIC FODER
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
